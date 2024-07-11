bc1827d635dab9fdd7828a219a65b6c19df28898
Jasim Uddin <jasimbdpro@gmail.com>
2024-07-11 19:41:09 +0600

    my first node js project api

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 const port = 3000
 
+//get  
+app.get('/name', (req, res) => {
+    res.send('Jasim Uddin khan')
+})
+
 app.get('/', (req, res) => {
-    res.send('Thank you for creating and starting the server 30000!')
+    const fruits = {
+        product: 'adafddgggfhhhhhh',
+        price: 200
+    }
+    res.send(fruits)
+})
+const users = ['Asaddgg', 'Moin', 'Sabeddd', 'Susmita', 'Sohana']
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+app.get('/users/:id', (req, res) => {
+    console.log(req.params)
+    const userId = req.params.id
+    const name = users[userId]
+    res.send({ name, userId })
+})
+
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({ fruit: 'banana', quantity: 10000 })
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    const user = req.body
+    user.id = 55
+    res.send(user)
 })
 
 app.listen(port, () => {
